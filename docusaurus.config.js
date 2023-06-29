3dc34eda2909dd9b5760d9e8e6ec781a1d62cc20
Fintan Bolton <techwriter@pure-corba.com>
2023-06-29 16:13:46 +0200

    Corrected syntax in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'fbolton', // Usually your GitHub org/user name.
   projectName: 'gh-writing-samples', // Usually your repo name.
-  trailingSlash: false
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
